917b4f7d23f474208b3bcf13f5601e9bb7beb4be
andrescjp <andres@cjp.mx>
2020-10-12 11:25:23 -0500

    pagina de login

--- resources/js/views/login/Login.vue
+++ resources/js/views/login/Login.vue
@@ -1,12 +1,31 @@
 <template>
-    
+    <div class="w-full h-screen flex justify-center content-center flex-wrap">
+        <div class="card xl:w-2/12 lg:w-4/12 md:w-6/12 sm:8/12 p-4">
+            <h1 class="title">ERP</h1>
+            <b-field label="Email">
+                <b-input v-model="email" type="email" icon="user" placeholder="Email"></b-input>
+            </b-field>
+            <b-field label="Contraseña">
+                <b-input v-model="password" type="password" icon="key" placeholder="Contraseña"></b-input>
+            </b-field>
+            <b-button type="is-primary" class="mt-5" icon-left="sign-in-alt" expanded>
+                Iniciar sesión
+            </b-button>
+        </div>
+    </div>
 </template>
 
 
 
 <script>
 export default {
-    
+    data() {
+        return {
+            email: null,
+            password: null,
+            remember: false
+        }
+    }
 }
 </script>
 
