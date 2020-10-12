d66a100dc649c9f68059834daf4a1b09182cf383
andrescjp <andres@cjp.mx>
2020-10-12 12:03:15 -0500

    componente titulo terminado

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="flex items-center" :class="sizeHeader">
+    <div class="flex items-center w-full" :class="sizeHeader">
             <b-icon :pack="iconPack" :icon="icon" v-if="icon"/>
-            <b-image :src="src" :rounded="roundedImage" v-if="image"/>
+            <b-image :src="src" :rounded="roundedImage" v-if="image" :class="imageClasses"/>
         <h1 class="title" :class="sizeHeader">
             {{ title }}
         </h1>
@@ -15,9 +15,10 @@ export default {
         size: { type: Number, default: 1 },
         icon: { type: String, default: null },
         image: { type: String, default: null },
-        imageSize: { type: String, default: 'auto' },
+        imageClasses: { type: String, default: null },
+        iconClasses: { type: String, default: null },
         iconPack: { type: String, default: 'fas' },
-        roundedImage: { type: Boolean, default: false}
+        roundedImage: { type: Boolean, default: false }
     },
     computed: {
         sizeHeader: function () {
--- resources/js/views/login/Login.vue
+++ resources/js/views/login/Login.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full h-screen flex justify-center content-center flex-wrap">
         <div class="card xl:w-2/12 lg:w-4/12 md:w-6/12 sm:8/12 p-4">
-            <b-header title="ERP" :size="1" icon="home"/>
+            <b-header title="ERP" :size="1" roundedImage/>
             <b-field label="Email">
                 <b-input v-model="email" type="email" icon="user" placeholder="Email"></b-input>
             </b-field>
