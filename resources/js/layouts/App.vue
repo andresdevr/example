6283d1823fa215fd24556bee121ed595c8d44d02
andrescjp <andres@cjp.mx>
2020-10-12 13:25:49 -0500

    listener de tamaño de ventana

--- resources/js/layouts/App.vue
+++ resources/js/layouts/App.vue
@@ -5,7 +5,17 @@
 
 <script>
 export default {
-
+    beforeCreate() {
+        this.$store.commit('setWidth', window.innerWidth || document.body.clientWidth);
+        this.$store.commit('setHeight', window.innerHeight || document.body.clientHeight);
+        window.onresize = () => this.listenBrowserSize();
+    },
+    methods: {
+        listenBrowserSize: function () {
+            this.$store.commit('setWidth', window.innerWidth || document.body.clientWidth);
+            this.$store.commit('setHeight', window.innerHeight || document.body.clientHeight);
+        }
+    }
 }
 </script>
 
--- resources/js/store/store.js
+++ resources/js/store/store.js
@@ -3,11 +3,27 @@ import { validations } from './modules/validations';
 
 export const vuex = {
     state: {
-        appName: 'ERP'
+        appName: 'ERP',
+        width: 0,
+        height: 0
+    },
+    mutations: {
+        setWidth(state, width) {
+            state.width = width;
+        },
+        setHeight(state, height) {
+            state.height = height;
+        }
     },
     getters: {
         appName: state => {
             return state.appName;
+        },
+        width: state => {
+            return state.width;
+        },
+        height: state => {
+            return state.height;
         }
     },
     modules: {
