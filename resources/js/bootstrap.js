2546020e3372e3495a27a7849ebc2ef0124375dc
andrescjp <andres@cjp.mx>
2020-10-12 12:54:28 -0500

    uso de credenciales

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,6 +9,7 @@ window._ = require('lodash');
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.withCredentials = true;
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -27,6 +28,4 @@ window.Echo = new Echo({
     forceTLS: true
 });
 
-var validator = require('validator');
-
-window.validator = validator;
+window.validator = require('validator');
