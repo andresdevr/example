76b8288077865bcc8ef8a5bc70d9fe6ab0e9bf3d
andrescjp <andres@cjp.mx>
2020-10-13 16:05:27 -0500

    listado de usuarios y detalle

--- resources/js/views/users/list/List.vue
+++ resources/js/views/users/list/List.vue
@@ -1,11 +1,48 @@
 <template class="container">
     <b-card class="xl:m-5 lg:m-4 md:m-3 m-2">
         <b-header title="Usuarios" :size="titleSize"/>
+
+        <b-tabs v-model="activeTab">
+            <b-tab-item label="Lista">
+
+                <users-table :data="users" v-if="users.length" @user-selected="setUser">
+                </users-table>
+                
+            </b-tab-item>
+
+            <b-tab-item label="Usuario Seleccionado">
+
+                <user-details :user="user">
+                </user-details>
+
+            </b-tab-item>
+            <b-tab-item label="Añadir">
+                
+                <create-user>
+                </create-user>
+
+            </b-tab-item>
+        </b-tabs>
     </b-card>
 </template>
 
 <script>
+import Table from './Table';
+import UserDetails from './UserDetails';
+import CreateUser from './CreateUser';
+
 export default {
+    components: {
+        'users-table': Table,
+        UserDetails,
+        CreateUser
+    },
+    data() {
+        return {
+            user: null,
+            activeTab: 0
+        }
+    },
     beforeCreate() {
         this.$store.dispatch('getUsers');
     },
@@ -21,6 +58,15 @@ export default {
             if (size == 'sm')
                 return 4;
             return 5;
+        },
+        users: function () {
+            return this.$store.getters.users;
+        }
+    },
+    methods: {
+        setUser: function (user) {
+            this.user = user;
+            this.activeTab = 1;
         }
     }
 }
---
+++ resources/js/views/users/list/Table.vue
@@ -0,0 +1,46 @@
+<template>
+    <b-table
+        :data="data"
+        :columns="columns"
+        :selected.sync="selected"
+        focusable>
+    </b-table>
+</template>
+
+<script>
+    export default {
+        props: ['data'],
+        data() {
+            return {
+                selected: {},
+                columns: [
+                    {
+                        field: 'id',
+                        label: 'ID',
+                        width: '40',
+                        numeric: true
+                    },
+                    {
+                        field: 'avatar',
+                        label: 'Avatar',
+                    },
+                    {
+                        field: 'name',
+                        label: 'Name',
+                    },
+                    {
+                        field: 'email',
+                        label: 'Email',
+                        centered: true
+                    }
+                ]
+            }
+        },
+        watch: {
+            selected: function (value) {
+                this.$emit('user-selected', value);
+            }
+        }
+
+    }
+</script>
