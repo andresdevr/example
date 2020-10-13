0292ea8bdb6f465ac77d89330bfb4a9b019f70a0
andrescjp <andres@cjp.mx>
2020-10-13 14:55:57 -0500

    color de fondo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,7 +19,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
+    <div id="app" class="bg-gray-200 bg-opacity-75">
         <app>
         </app>
     </div>
