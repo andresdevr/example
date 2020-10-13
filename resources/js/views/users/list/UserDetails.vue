2641ca4551661bebdde4b4a3004a0fe3c04033ac
andrescjp <andres@cjp.mx>
2020-10-13 17:48:10 -0500

    editar y ver permisos

--- resources/js/views/users/list/UserDetails.vue
+++ resources/js/views/users/list/UserDetails.vue
@@ -4,12 +4,20 @@
             <b-image :src="image" rounded>
             </b-image>
         </div>
-            <h3 class="text-center is-size-3">
-                {{ name }}
-            </h3>
-            <h4 class="text-center is-size-4">
-                {{ email }}
-            </h4>
+        <h3 class="text-center is-size-3">
+            {{ name }}
+        </h3>
+        <h4 class="text-center is-size-4">
+            {{ email }}
+        </h4>
+        <div class="w-full flex justify-center" v-if="email">
+            <b-button type="is-info" class="mr-2">
+                Editar
+            </b-button>
+            <b-button type="is-warning" class="ml-2">
+                Permisos
+            </b-button>
+        </div>
     </div>
 </template>
 
