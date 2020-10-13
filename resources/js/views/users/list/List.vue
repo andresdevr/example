5063d6f076bb804344f8ec560ecfb1c868b3c407
andrescjp <andres@cjp.mx>
2020-10-13 15:18:53 -0500

    titulo responsivo

--- resources/js/views/users/list/List.vue
+++ resources/js/views/users/list/List.vue
@@ -1,6 +1,6 @@
 <template class="container">
     <b-card class="xl:m-5 lg:m-4 md:m-3 m-2">
-        <b-header title="Usuarios"/>
+        <b-header title="Usuarios" :size="titleSize"/>
     </b-card>
 </template>
 
@@ -8,6 +8,20 @@
 export default {
     beforeCreate() {
         this.$store.dispatch('getUsers');
+    },
+    computed: {
+        titleSize: function () {
+            var size = this.$store.getters.media;
+            if (size == 'xl')
+                return 1;
+            if (size == 'lg')
+                return 2;
+            if (size == 'md')
+                return 3;
+            if (size == 'sm')
+                return 4;
+            return 5;
+        }
     }
 }
 </script>
