cf78e2d86d6a4b9fcfe326694d033214b2029745
andrescjp <andres@cjp.mx>
2020-10-14 12:59:21 -0500

    ruta para permisos

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,16 @@ use Illuminate\Support\Facades\Route;
 // auth routes
 Auth::routes(['verify' => true, 'register' => false]);
 
-//example
 
 
-Route::prefix('ajax')->group(function () {
+Route::prefix('ajax')->middleware('ajax')->group(function () {
+    
+    //example
+    Route::apiResource('products', ProductController::class);
 
-    Route::apiResource('products', ProductController::class)->middleware('ajax');
-    Route::apiResource('users', UserController::class)->middleware('ajax');
+    Route::apiResource('users', UserController::class);
+   Route::get('permissions', PermissionController::class)->name('permissions.index');
+
 });
 
 
