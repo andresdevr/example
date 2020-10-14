6d97e2558d09d9e021812d3d4208caf4a04bc7b0
andrescjp <andres@cjp.mx>
2020-10-14 11:31:11 -0500

    correccion de avatar

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable implements PresenterInterface, MustVerifyEmai
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'avatar'
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('avatart')->nullable();
+            $table->string('avatar')->nullable();
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
