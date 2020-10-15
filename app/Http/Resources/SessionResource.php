7973628f40340bf9cb06e83b0209cc7f9a1de431
andrescjp <andres@cjp.mx>
2020-10-15 13:04:23 -0500

    recurso creado de sesion

--- app/Http/Resources/SessionResource.php
+++ app/Http/Resources/SessionResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class SessionResource extends JsonResource
@@ -14,6 +15,10 @@ class SessionResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'ip' => $this->ip_address,
+            'last_activity' => Carbon::createFromTimestamp($this->last_activity)->toDateTimeString()
+
+        ];
     }
 }
